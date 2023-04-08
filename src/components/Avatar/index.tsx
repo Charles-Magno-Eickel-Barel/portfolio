0703f00102213288a0fe9e1845da6a34a145f0e1
seu-usersaneme <seu-email>
2023-04-07 23:05:32 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/Charles-Magno-Eickel-Barel.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Siderbar = () => (
   <aside>
-    <img src="https://github.com/Charles-Magno-Eickel-Barel.png" />
-    <Titulo fontSize={20}>Charles Magno Eickel Barel</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Charles Magno Eickel Barel</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Charles-Magno-Eickel-Barel
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Web
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
